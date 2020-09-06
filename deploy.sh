953cac0666ddc1cfae8d8d4581cf22939c51cef2
eyuelberga <eyuel.berga@yahoo.com>
2020-09-06 17:15:33 +0300

    :up: modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:eyuelberga/vue-leaflet-geolocation-selector master:gh-pages
+git push -f git@github.com:eyuelberga/vue-leaflet-geolocation-selector.git master:gh-pages
 cd -
